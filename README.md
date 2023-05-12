3dc1011956e6d1b8b38f5a6e3ce950ad36c3948a
graef <simonjn45@gmail.com>
2023-05-12 19:48:11 +0200

    Small additions to main and README

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 A bad MOS 6502 emulator
 
 ## References:
-* [1] http://emulator101.com/
-* [2] https://www.masswerk.at/6502/6502_instruction_set.html
-* [3] https://blog.scottlogic.com/2020/08/26/codegen-6502-webassembly.html
-* [4] https://codeburst.io/an-introduction-to-6502-assembly-and-low-level-programming-7c11fa6b9cb9
-* [5] https://youtu.be/m6l3Elk7-Hg
-* [6] https://github.com/jfoucher/rust-6502
-* [7] https://rosettacode.org/wiki/Category:6502_Assembly#Assembler_Syntax
-* [8] https://github.com/Klaus2m5/6502_65C02_functional_tests/tree/master
+* [1] https://en.wikipedia.org/wiki/MOS_Technology_6502
+* [2] http://emulator101.com/
+* [3] https://www.masswerk.at/6502/6502_instruction_set.html
+* [4] https://blog.scottlogic.com/2020/08/26/codegen-6502-webassembly.html
+* [5] https://codeburst.io/an-introduction-to-6502-assembly-and-low-level-programming-7c11fa6b9cb9
+* [6] https://youtu.be/m6l3Elk7-Hg
+* [7] https://github.com/jfoucher/rust-6502
+* [8] https://rosettacode.org/wiki/Category:6502_Assembly#Assembler_Syntax
+* [9] https://github.com/Klaus2m5/6502_65C02_functional_tests/tree/master
+* [10] http://www.6502.org/
+* [11] https://www.middle-engine.com/blog/posts/2020/06/23/programming-the-nes-the-6502-in-detail
+* [12] https://github.com/f0lg0/6502
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,53 @@
-#[derive(Debug)]
-struct Chip {
+const MEMORY: usize = 65536;
 
+// The flag for the flag in the Chip
+enum Flag {
+    N = 0x80, // [1000 0000] negative
+    V = 0x40, // [0100 0000] overflow
+              // [0010 0000] Reserved
+    B = 0x10, // [0001 0000] break
+    D = 0x08, // [0000 1000] decimale
+    I = 0x04, // [0000 0100] interrpt disable
+    Z = 0x02, // [0000 0010] zero
+    C = 0x00, // [0000 0001] carry
+}
+
+struct Chip {
+    // Registers:
+    // Accumulator:
+    pub acc: u8,
+    // Index's x and y:
+    pub rx: u8,
+    pub ry: u8,
+    // Process Status flag:
+    pub f: Flag,
+    // Stack Pointer:
+    pub sp: u8,
+    // Program Counter:
+    pub pc: u16,
+    // TODO: Putting Memory in it's own struct
+    // Memory:
+    // RESERVED: 256 bytes 0x0000 to 0x00FF -> Zero Page
+    // RESERVED: 256 bytes 0x0100 to 0x01FF -> System Stack
+    // PROGRAM DATA: 0x10000 - 0x206
+    // RESERVED: last 6 bytes of memory
+    pub memory: [u8; MEMORY],
 }
 
 impl Chip {
     pub fn new() -> Chip {
         Chip {
-
+            acc: 0,
+            rx: 0,
+            ry: 0,
+            f: Flag::Z,
+            sp: 0,
+            pc:0,
+            memory: [0; MEMORY]
         }
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let c = Chip::new();
-
-    println!("{:?}", c);
 }
